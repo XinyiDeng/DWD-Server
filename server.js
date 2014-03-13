4ee086003b62e0543855349727b89ae6296fe62c
Xinyi Deng <likeydeng@yeah.net>
2014-03-12 20:49:49 -0400

    Update

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Hello, world')
+//console.log('Hello, world')
+
+var http = require('http');
+console.log('Hello, starting server...');
+var server = http.createServer(function(req, res){
+  res.writeHead(200,{'content-Type': 'text/html'});
+  res.end('<h1>Hello, World</h1>');
+  });
+var port = Number(process.env.PORT || 5000);
+console.log('Listening on port', port);
+server.listen(port);
